fc420113902324f9ea0f2def41772dfcbb835f1c
User <paulate.alonso@gmail.com>
2022-10-22 13:07:43 +0200

    Exercise 7

---
+++ Exercise 1/main.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+let cheeseCalories = 200;
+let eggsRequired = 4;
+let caloriesPerEgg = 75;
+let eggsCalories = caloriesPerEgg*eggsRequired
+let omeletteDuFromageCalories = cheeseCalories+eggsCalories
+
+
+console.log("la cantidad total de calorías en los huevos es de ", eggsCalories)
+console.log("las calorías totales son", omeletteDuFromageCalories)
+console.log(omeletteDuFromageCalories/2)
+
---
+++ Exercise 2/main2.js
@@ -0,0 +1,24 @@
+let turtle1 = "Leonardo ";
+let turtle2 = "Raphael ";
+let turtle3 = "Donatello ";
+let turtle4 = "Michelangelo ";
+let allTurtles = ""
+
+allTurtles += turtle1
+
+console.log("el primero de todas las tortugas es", allTurtles)
+
+allTurtles += turtle2
+
+console.log("el primero y segundo de las tortugas es", allTurtles)
+
+allTurtles += turtle3
+
+console.log("el primero, segundo y tercero de las tortugas es", allTurtles)
+
+allTurtles += turtle4
+
+console.log("todas las tortugas son", allTurtles)
+
+
+console.log(message.indexOf(""))
